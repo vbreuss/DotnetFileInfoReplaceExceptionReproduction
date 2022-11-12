7bf70e86a1c1e117adabf84bad3a2fe521b5ca12
Valentin Breuß <vbreuss@gmail.com>
2022-11-12 20:21:00 +0100

    Add additional test

--- DotnetFileSystemExceptions.Tests/FileInfoReplaceTests.cs
+++ DotnetFileSystemExceptions.Tests/FileInfoReplaceTests.cs
@@ -5,7 +5,7 @@ namespace DotnetFileSystemExceptions.Tests;
 public class FileInfoReplaceTests
 {
 	[Fact]
-	public void Replace_DirectoryMissing_ShouldThrow()
+	public void Replace_DestinationDirectoryMissing_ShouldThrow()
 	{
 		using (Initialize.TemporaryDirectory())
 		{
@@ -19,14 +19,7 @@ public class FileInfoReplaceTests
 
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
 			{
-				if (Test.IsNet7)
-				{
-					Assert.IsType<DirectoryNotFoundException>(exception);
-				}
-				else
-				{
-					Assert.IsType<FileNotFoundException>(exception);
-				}
+				Assert.IsType<FileNotFoundException>(exception);
 			}
 
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
@@ -37,6 +30,35 @@ public class FileInfoReplaceTests
 			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 			{
 				Assert.IsType<DirectoryNotFoundException>(exception);
+			}
+		}
+	}
+
+	[Fact]
+	public void Replace_SourceDirectoryMissing_ShouldThrow()
+	{
+		using (Initialize.TemporaryDirectory())
+		{
+			string sourcePath = Path.Combine(Directory.GetCurrentDirectory(),
+				"not-existing-directory",
+				"source.txt");
+			FileInfo sut = new(sourcePath);
+
+			Exception? exception = Record.Exception(() => sut.Replace("destination.txt", null));
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+			{
+				Assert.IsType<FileNotFoundException>(exception);
+			}
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+			{
+				Assert.IsType<FileNotFoundException>(exception);
+			}
+
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				Assert.IsType<FileNotFoundException>(exception);
 			}
 		}
 	}
